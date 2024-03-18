dc1383cfe3adfb62b4a2bf489aeeddbdeac479d1
Reyad Khan <khanreyad207@gmail.com>
2024-03-19 02:48:35 +0600

    19.03.24 commit

---
+++ js_prt.js
@@ -0,0 +1,8 @@
+var studends = ['Reyad','Ontora','Fahim','Abdullha','Shofiullha'];
+let findValue = studends.indexOf('Abdullha');
+console.log(findValue);
+
+// I will Learn : 
+// Array Find Methods:
+// https://www.w3schools.com/jsref/jsref_indexof_array.asp
+// https://www.youtube.com/watch?v=76XQb3CXCvA&list=PLerpoOYRrjUz_yFaxJ9wJ0JqCqPIt9ulJ&index=11
