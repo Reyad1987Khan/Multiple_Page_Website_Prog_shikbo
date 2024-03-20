5c65fb5b359fcea77074d5801e7ecddb177eee74
Reyad Khan <khanreyad207@gmail.com>
2024-03-20 16:03:32 +0600

    Footer Completed commit

--- js_prt.js
+++ js_prt.js
@@ -1,8 +1,15 @@
 var studends = ['Reyad','Ontora','Fahim','Abdullha','Shofiullha'];
 let findValue = studends.indexOf('Abdullha');
-console.log(findValue);
+let indexvalue = studends[3];
+studends[3] = 'Radip';
+let change = studends[3] ;
+console.log(indexvalue);
+console.log(studends);
+console.log(change);
 
 // I will Learn : 
 // Array Find Methods:
 // https://www.w3schools.com/jsref/jsref_indexof_array.asp
-// https://www.youtube.com/watch?v=76XQb3CXCvA&list=PLerpoOYRrjUz_yFaxJ9wJ0JqCqPIt9ulJ&index=11
+
+// Learn Continou 
+// https://www.youtube.com/watch?v=NOh8iTIO5d4&list=PLerpoOYRrjUz_yFaxJ9wJ0JqCqPIt9ulJ&index=13
