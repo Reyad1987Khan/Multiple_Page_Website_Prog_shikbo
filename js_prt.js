c643a82d3e3f6b4fbd5bb7042f6764e755007e4d
Reyad Khan <khanreyad207@gmail.com>
2024-03-25 02:25:48 +0600

    Before Final commit

--- js_prt.js
+++ js_prt.js
@@ -1,15 +1,56 @@
-var studends = ['Reyad','Ontora','Fahim','Abdullha','Shofiullha'];
-let findValue = studends.indexOf('Abdullha');
-let indexvalue = studends[3];
-studends[3] = 'Radip';
-let change = studends[3] ;
-console.log(indexvalue);
-console.log(studends);
-console.log(change);
+
+//  var studentName = ['Barsha','Samiha','eva','Rifah','Nafisa','Syeda','Mashrar','Bushra','Munazza','Syeda','Moon','Fahmida','Amatullah','Syeda','Jerin','Emu','Aysha','Mahveen'];
+// let students = studentName.find((currentValue,currentIndex,arr) =>{
+//     return currentValue == "Fahmida";
+// },this);
+// console.log(students);
+// https://www.youtube.com/watch?v=UeMhY-8k1E4
+// class Student {
+//     constructor(name,age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+//     test(){
+//         console.log("Hello Test Function!");
+//     };
+//     exampleFunction (){
+//         let myArray = [1,2,3,4,5 ];
+//         myArray.find(function(){
+//                 this.test();
+//         },this);
+//     }
+// }
+
+// let student = new Student('Reyad',37);
+// student.exampleFunction();
+
+// https://www.youtube.com/watch?v=-x5qNV3s-rA 
+// var studentName = ['Barsha','Samiha','eva','Rifah','Nafisa','Syeda','Mashrar','Bushra','Munazza','Syeda','Moon','Fahmida','Amatullah','Syeda','Jerin','Emu','Aysha','Mahveen'];
+// let getName = studentName.find(currentValue=>{
+//     return currentValue == "Emu";
+// });
+// console.log(getName);
+// const studentResult = [
+//     {name: 'Reyad Khan',  number: 79, subject: 'Math'},
+//     {name: 'Ontora', number: 85, subject: 'Math'},
+//     {name: 'Fahim  ', number: 80, subject: 'English'},
+//     {name: 'Abdullha', number: 33, subject: 'Bangla'},
+//     {name: 'Sofiullha', number: 80, subject: 'Bangla'},
+//     {name: 'Tonima', number: 20, subject: 'Bangla'},
+// ];
+// let getResult = studentResult.filter((value,index,arr) => {
+//             return arr.indexOf(value) === index;
+// });
+// console.log(getResult);
+
+
+var studentName = ['Barsha','Samiha','eva','Rifah','Nafisa','Syeda','Mashrar','Bushra','Munazza','Syeda','Moon','Fahmida','Amatullah','Syeda','Jerin','Emu','Aysha','Mahveen'];
+studentName.sort();
+console.log(studentName);
 
 // I will Learn : 
 // Array Find Methods:
 // https://www.w3schools.com/jsref/jsref_indexof_array.asp
 
 // Learn Continou 
-// https://www.youtube.com/watch?v=NOh8iTIO5d4&list=PLerpoOYRrjUz_yFaxJ9wJ0JqCqPIt9ulJ&index=13
+// https://www.youtube.com/watch?v=Fj4PNqZUfCc&list=PLerpoOYRrjUz_yFaxJ9wJ0JqCqPIt9ulJ&index=15
